2af1f0ed755f193a04c240e4be814144a69397c1
rashmi-nair-here <166900815+rashmi-nair-here@users.noreply.github.com>
2026-01-08 00:18:52 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-fetch("rashmi-nair-here/unclassroom/written-works/posts.json")
+fetch("unclassroom/written-works/posts.json")
 .then(res => res.json())
 .then(posts => {
     const articles = document.querySelectorAll(".post");
@@ -23,6 +23,7 @@ fetch("rashmi-nair-here/unclassroom/written-works/posts.json")
     });
 
 })
+
 
 
 
